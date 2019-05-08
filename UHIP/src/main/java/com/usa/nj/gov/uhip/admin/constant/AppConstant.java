5e59112ce527184ad2a8248cff24f0ce25f19e2c
Avinash <Avinash@DESKTOP-0HCK08T>
2019-05-08 23:28:29 +0530

    add AppConstant class

---
+++ UHIP/src/main/java/com/usa/nj/gov/uhip/admin/constant/AppConstant.java
@@ -0,0 +1,11 @@
+package com.usa.nj.gov.uhip.admin.constant;
+
+/**
+ * Here,This is the class all hard code value added in project
+ * @author Avinash
+ *
+ */
+public class AppConstant {
+     public static final String ERROR = "error";
+     public static final String ERROR_MESSAGE = "Sorry we are unable to display source page";
+}
